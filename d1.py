54017248c7c9620934eae0c2e81b81c74b90e2c3
mewmew <rnd0x00@gmail.com>
2018-04-21 02:50:33 +0200

    typ: convert types from notes repo into Python representation

--- d1.py
+++ d1.py
@@ -2,7 +2,11 @@
 
 # Script and python library for interacting with Diablo 1 from Python.
 
+from io import BytesIO
+
 import hook
+from typ._types import ffi
+from typ._types import lib as types
 
 # Executable name.
 djavul_exe_name = "djavul.exe"
@@ -13,6 +17,32 @@ player_size = 0x54D8
 
 
 def get_player(player_num=0):
+	'''
+	Return the given player struct.
+
+	player_num -- player number in range [0, 4)
+	'''
+
+	player_buf = get_player_content(player_num)
+	player = ffi.new('Player *')
+	stream = BytesIO(player_buf)
+	stream.readinto(ffi.buffer(player))
+	return player
+
+
+def set_player(player, player_num=0):
+	'''
+	Set the given player struct.
+
+	player     -- contents of the player struct
+	player_num -- player number in range [0, 4)
+	'''
+
+	player_buf = ffi.buffer(player)[:]
+	set_player_content(player_buf, player_num)
+
+
+def get_player_content(player_num=0):
 	'''
 	Return the contents of the given player struct.
 
@@ -26,17 +56,16 @@ def get_player(player_num=0):
 		return bytearray(buf)
 
 
-def set_player(player, player_num=0):
+def set_player_content(player_buf, player_num=0):
 	'''
 	Set the contents of the given player struct.
 
 	player     -- contents of the player struct
 	player_num -- player number in range [0, 4)
 	'''
-	assert len(player) == player_size
+	assert len(player_buf) == player_size
 	assert 0 <= player_num < 4
 
 	with hook.Process(djavul_exe_name) as p:
 		addr = players_addr + player_size*player_num
-		buf = p.write_mem(addr, player)
-		return buf
+		p.write_mem(addr, player_buf)
---
+++ typ/gen_types.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import cffi
+
+def gen_types():
+	dir = os.path.dirname(__file__)
+	types_h_path = os.path.join(dir, "types.h.pre")
+	with open(types_h_path) as f:
+		ffi = cffi.FFI()
+		ffi.cdef(f.read())
+		ffi.set_source(
+			"_types",
+			'#include "types.h"',
+		)
+		ffi.compile()
+
+if __name__ == '__main__':
+	gen_types()
