cec4d4e2c666541533394f7a6d72b21fe08ca510
mewmew <rnd0x00@gmail.com>
2018-04-25 01:59:06 +0200

    types: generate cffi binding for rdata, data and bss .h files

--- typ/gen_types.py
+++ typ/gen_types.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python2
 
-import os
 import cffi
+import os
+
+def get_sources():
+	sources = []
+	for dir in ['notes/rdata', 'notes/data', 'notes/bss']:
+		sources += [os.path.join(dir, x) for x in os.listdir(dir) if x.endswith('.h')]
+	return sources
+
+def file_get_content(path):
+	return open(path).read()
 
 def gen_types():
+	ffi = cffi.FFI()
+
+	# Parse types.h
 	dir = os.path.dirname(__file__)
-	types_h_path = os.path.join(dir, "types.h.pre")
-	with open(types_h_path) as f:
-		ffi = cffi.FFI()
-		ffi.cdef(f.read())
-		ffi.set_source(
-			"_types",
-			'#include "types.h"',
-		)
-		ffi.compile()
+	types_h_path = os.path.join(dir, 'notes/include/types.h.pre')
+	types_h_buf = file_get_content(types_h_path)
+	ffi.cdef(types_h_buf)
+	sources_buf = '#include "types.h"\n'
+
+	# Parse rdata, data and bss .h files
+	for path in get_sources():
+		buf = file_get_content(path)
+		ffi.cdef(buf)
+		sources_buf += '#include "{}"\n'.format(path)
+	ffi.set_source(
+		'_types',
+		sources_buf,
+		include_dirs=['notes/include'],
+	)
+	ffi.compile()
 
 if __name__ == '__main__':
 	gen_types()
